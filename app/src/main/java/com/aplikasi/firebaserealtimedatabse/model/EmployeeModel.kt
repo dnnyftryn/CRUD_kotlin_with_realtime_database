f1aa92d8f6229f3aaf8c797d1d805b5f934a785e
dnny_ftryn <dnny.ftryn0901@gmail.com>
2022-11-03 15:30:11 +0700

    this app not use binding

---
+++ app/src/main/java/com/aplikasi/firebaserealtimedatabse/model/EmployeeModel.kt
@@ -0,0 +1,9 @@
+package com.aplikasi.firebaserealtimedatabse.model
+
+
+data class EmployeeModel(
+    var empId: String? = null,
+    var empName: String? = null,
+    var empAge: String? = null,
+    var empSalary: String? = null
+)
